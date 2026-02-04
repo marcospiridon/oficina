d6e7003064f454c2a229ae38c3bd23f55ae08608
Marco Fernandes <marcospiridon@yahoo.com>
2026-02-04 09:52:54 +0000

    More seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,5 +12,19 @@ admin.workshop = main_workshop
 admin.system_admin = true
 admin.save!
 
+# Create a fake workshop
+fake_workshop = Workshop.find_or_create_by!(slug: 'oficina-do-ze') do |w|
+  w.name = "Oficina do Zé"
+end
+
+# Create a fake user
+fake_user = User.find_or_initialize_by(email: "mecanico@oficina-do-ze.pt")
+fake_user.password = "123123123"
+fake_user.password_confirmation = "123123123"
+fake_user.workshop = fake_workshop
+fake_user.save!
+
 puts "Created Admin User: #{admin_email} / password123"
 puts "Created Default Workshop: #{main_workshop.name}"
+puts "Created Fake Workshop: #{fake_workshop.name}"
+puts "Created Fake User: #{fake_user.email} / 123123123"
